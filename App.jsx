7373b094e509024d04141f182506f7fd32223754
mitchelsevere <mitchelsevere@gmail.com>
2019-10-24 09:54:06 -0400

    css and style-loader for webpack

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import Minesweeper from "./components/Minesweeper";
 
 ReactDOM.render(<Minesweeper />, document.getElementById("root"));
